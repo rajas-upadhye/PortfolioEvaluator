93f49e403992c34cd3e93a78f25861c9a2046ef8
Rajas Upadhye <rajas151190@gmail.com>
2017-03-17 21:04:55 -0700

    Initial commit : Creating new repository

---
+++ src/main/java/com/Evaluator.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rajasupadhye on 3/17/17.
+ */
+public class Evaluator {
+    public static void main (String args[]){
+        Logger log = LoggerFactory.getLogger(Evaluator.class);
+        log.info("Starting Portfolio Analysis at : " + System.currentTimeMillis()/1000);
+
+    }
+}
